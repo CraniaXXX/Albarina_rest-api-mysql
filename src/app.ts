06623bd8f6255faea7a174b5dee517ddb646f40a
christian rey albarina <christianreyalbarina5@gmail.com>
2024-03-08 15:05:47 +0800

    first commit

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import  express from "express"
+import express from "express"
 import * as dotevnv from "dotenv"
 import cors from "cors"
 import helmet from "helmet"
@@ -10,6 +10,7 @@ dotevnv.config()
 if(!process.env.PORT){
     console.log(`No port value specified...`)
 }
+
 
 const PORT = parseInt(process.env.PORT as string, 10)
 
